e0f6fa08760aae1a5cfa39e6f395c55bb1585f15
Coolsheru2526 <shreyanshmishra.4365@gmail.com>
2024-07-21 19:58:25 +0530

    backend setup done

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,34 @@
+import { errorHandler } from "../utills/error.js";
+import bcryptjs from 'bcryptjs';
+import User from '../models/user.model.js';
+
+
+export const signup = async (req, res,next) => {
+  const { username, email, password } = req.body;
+
+  if (
+    !username ||
+    !email ||
+    !password ||
+    username === '' ||
+    email === '' ||
+    password === ''
+  ) {
+    next(errorHandler(400,'All fields required'));
+  }
+
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+
+  const newUser = new User({
+    username,
+    email,
+    password: hashedPassword,
+  });
+
+  try {
+    await newUser.save();
+    res.json('Signup successful');
+  } catch (error) {
+    next(error);
+  }
+};
